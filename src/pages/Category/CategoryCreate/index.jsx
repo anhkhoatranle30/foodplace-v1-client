5a4be84c480dc61337f943a3b62438e51d277544
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2021-09-03 16:10:26 +0700

    Delete watch warning inside CategoryCreate for production

--- src/pages/Category/CategoryCreate/index.jsx
+++ src/pages/Category/CategoryCreate/index.jsx
@@ -15,7 +15,6 @@ export default function CategoryCreate() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(categorySchema),
