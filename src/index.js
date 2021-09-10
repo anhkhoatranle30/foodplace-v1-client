1f78fcd87e681007871d8ef3d041578ecfafe6bf
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2021-09-10 11:20:18 +0700

    Change from App.js to App.jsx + rules for eslint allow both .js and .jsx file extension

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import store from "./app/store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './app/store';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
