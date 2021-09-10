5c9b6212443fbc25241cd10e7dd62053fe3b258f
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2021-09-10 11:11:18 +0700

    turn off param reassign in eslint + fix error in login thunk api from return response.data.error to response.data (this is more correct)

--- src/app/slices/userSlice.js
+++ src/app/slices/userSlice.js
@@ -1,8 +1,8 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import userApi from "../../apis/userApi";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import userApi from '../../apis/userApi';
 
 export const loginUserAction = createAsyncThunk(
-  "users/login",
+  'users/login',
   async ({ email, password }, thunkApi) => {
     try {
       const response = await userApi.login(email, password);
@@ -11,13 +11,13 @@ export const loginUserAction = createAsyncThunk(
       if (!err.response) {
         throw err;
       }
-      return thunkApi.rejectWithValue(err.response.data.error);
+      return thunkApi.rejectWithValue(err.response.data);
     }
-  }
+  },
 );
 
 export const registerUserAction = createAsyncThunk(
-  "users/register",
+  'users/register',
   async ({ email, password }, thunkApi) => {
     try {
       const response = await userApi.register(email, password);
@@ -28,51 +28,45 @@ export const registerUserAction = createAsyncThunk(
       }
       return thunkApi.rejectWithValue(err.response.data);
     }
-  }
+  },
 );
 
 const initialState = {
-  status: "idle",
+  status: 'idle',
   data: {},
   token: null,
   error: null,
 };
 export const userSlice = createSlice({
-  name: "user",
+  name: 'user',
   initialState,
   reducers: {
-    logoutUserAction: (state) => {
-      // state.status = "idle";
-      // state.data = {};
-      // state.token = null;
-      // state.error = null;
-      return initialState;
-    },
+    logoutUserAction: () => initialState,
   },
   extraReducers: {
-    [loginUserAction.pending]: (state, action) => {
-      state.status = "loading";
+    [loginUserAction.pending]: (state) => {
+      state.status = 'loading';
     },
     [loginUserAction.fulfilled]: (state, action) => {
-      state.status = "success";
+      state.status = 'success';
       state.data = action.payload.user;
       state.token = action.payload.token;
       state.error = null;
     },
     [loginUserAction.rejected]: (state, action) => {
-      state.status = "error";
+      state.status = 'error';
       state.error = action.payload;
     },
-    [registerUserAction.pending]: (state, action) => {
-      state.status = "loading";
+    [registerUserAction.pending]: (state) => {
+      state.status = 'loading';
     },
     [registerUserAction.fulfilled]: (state, action) => {
-      state.status = "success";
+      state.status = 'success';
       state.data = action.payload.user;
       state.token = action.payload.token;
     },
     [registerUserAction.rejected]: (state, action) => {
-      state.status = "error";
+      state.status = 'error';
       state.error = action.payload;
     },
   },
