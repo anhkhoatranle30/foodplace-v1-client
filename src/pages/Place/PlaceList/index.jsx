cc32bf97e53c57bd19149025daa191c1558501fd
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2021-11-04 16:59:16 +0700

    Added a search input to PlaceList page

--- src/pages/Place/PlaceList/index.jsx
+++ src/pages/Place/PlaceList/index.jsx
@@ -1,15 +1,17 @@
 import {
-	Box,
-	Button,
-	Container,
-	FormControl,
-	Grid,
-	InputLabel,
-	MenuItem,
-	Select,
+  Box,
+  Button,
+  Container,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  OutlinedInput,
+  IconButton,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Add } from '@material-ui/icons';
+import { Add, Search } from '@material-ui/icons';
 import { Pagination } from '@material-ui/lab';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
@@ -17,168 +19,196 @@ import { Link } from 'react-router-dom';
 import placeApi from './../../../apis/placeApi';
 import PlaceCard from './../../../components/PlaceCard';
 import EmptyPlaceholder from '../../../components/EmptyPlaceholder';
+import { useForm } from 'react-hook-form';
 
 const useStyles = makeStyles({
-	formControl: {
-		minWidth: '120px',
-	},
+  formControl: {
+    minWidth: '120px',
+  },
 });
 
 export default function PlacesList() {
-	const classes = useStyles();
-	const categories = useSelector((state) => state.category.data);
-	const token = useSelector((state) => state.user.token);
-	const [currentPage, setCurrentPage] = useState(1);
-	const [places, setPlaces] = useState([]);
-	const [quantity, setQuantity] = useState(1);
-	const [isLoading, setIsLoading] = useState(false);
-	useEffect(() => {
-		async function fetchQuantity() {
-			try {
-				const response = await placeApi.fetchQuantity(token);
-				setQuantity(response.data.quantity);
-			} catch (error) {
-				window.alert(error.response.data);
-			}
-		}
+  const classes = useStyles();
 
-		fetchQuantity();
-	}, [token]);
-	useEffect(() => {
-		async function fetchPlaces() {
-			setIsLoading(true);
-			try {
-				const placesResponse = await placeApi.fetchAll(token, {
-					skip: itemsEachPage * (currentPage - 1),
-					limit: itemsEachPage,
-				});
-				setPlaces(placesResponse.data);
-				setIsLoading(false);
-			} catch (error) {
-				console.log(error.response.data);
-				setIsLoading(false);
-			}
-		}
+  //#region useForm
+  const { register, handleSubmit } = useForm();
+  //#endregion
 
-		fetchPlaces();
-	}, [token, currentPage]);
+  //#region redux hooks
+  const categories = useSelector((state) => state.category.data);
+  const token = useSelector((state) => state.user.token);
+  //#endregion
 
-	const itemsEachPage = 12;
-	const countPage = () => {
-		const floor = Math.floor(quantity / itemsEachPage);
-		if (floor === Math.ceil(quantity / itemsEachPage)) {
-			return floor;
-		}
-		return floor + 1;
-	};
+  //#region useState (searchString, currentPage, places, quantity, isLoading)
+  const [searchString, setSearchString] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [places, setPlaces] = useState([]);
+  const [quantity, setQuantity] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  //#endregion
 
-	const renderPlaces = () => {
-		if (places.length === 0) {
-			return (
-				<EmptyPlaceholder
-					title="No place found!"
-					body="It seems like you have not added any place yet. Click the `Add a place` button to see more."
-				/>
-			);
-		}
-		return places.map((place) => (
-			<Grid key={place._id} item xs={12} sm={6} md={4} lg={3}>
-				<PlaceCard place={place} />
-			</Grid>
-		));
-	};
+  //#region useEffect(quantity, places)
+  useEffect(() => {
+    async function fetchQuantity() {
+      try {
+        const response = await placeApi.fetchQuantity(token);
+        setQuantity(response.data.quantity);
+      } catch (error) {
+        window.alert(error.response.data);
+      }
+    }
 
-	const renderSkeletonPlaces = () => {
-		const result = [];
-		for (let i = 0; i < 1; i++) {
-			result.push(
-				<Grid
-					key={`place-card-skeleton-${i}`}
-					item
-					xs={12}
-					sm={6}
-					md={4}
-					lg={3}
-				>
-					<PlaceCard isSkeleton />
-				</Grid>
-			);
-		}
-		return result;
-	};
+    fetchQuantity();
+  }, [token]);
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsLoading(true);
+      try {
+        const placesResponse = await placeApi.fetchAll(token, {
+          skip: itemsEachPage * (currentPage - 1),
+          limit: itemsEachPage,
+        });
+        setPlaces(placesResponse.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.response.data);
+        setIsLoading(false);
+      }
+    }
 
-	const renderCategories = (categoriesArray) => {
-		const result = [];
-		result.push(
-			<MenuItem key="all_categories" value="">
+    fetchPlaces();
+  }, [token, currentPage]);
+  //#endregion
+
+  const itemsEachPage = 12;
+  const countPage = () => {
+    const floor = Math.floor(quantity / itemsEachPage);
+    if (floor === Math.ceil(quantity / itemsEachPage)) {
+      return floor;
+    }
+    return floor + 1;
+  };
+
+  //#region render functions (places, skeleton, categories)
+  const renderPlaces = () => {
+    if (places.length === 0) {
+      return (
+        <EmptyPlaceholder
+          title="No place found!"
+          body="It seems like you have not added any place yet. Click the `Add a place` button to see more."
+        />
+      );
+    }
+    return places.map((place) => (
+      <Grid key={place._id} item xs={12} sm={6} md={4} lg={3}>
+        <PlaceCard place={place} />
+      </Grid>
+    ));
+  };
+
+  const renderSkeletonPlaces = () => {
+    const result = [];
+    for (let i = 0; i < 1; i++) {
+      result.push(
+        <Grid
+          key={`place-card-skeleton-${i}`}
+          item
+          xs={12}
+          sm={6}
+          md={4}
+          lg={3}
+        >
+          <PlaceCard isSkeleton />
+        </Grid>
+      );
+    }
+    return result;
+  };
+
+  const renderCategories = (categoriesArray) => {
+    const result = [];
+    result.push(
+      <MenuItem key="all_categories" value="">
         All
-			</MenuItem>
-		);
-		const options = categoriesArray.map((category) => (
-			<MenuItem key={category._id} value={category._id}>
-				{category.name}
-			</MenuItem>
-		));
-		result.push(...options);
-		return result;
-	};
+      </MenuItem>
+    );
+    const options = categoriesArray.map((category) => (
+      <MenuItem key={category._id} value={category._id}>
+        {category.name}
+      </MenuItem>
+    ));
+    result.push(...options);
+    return result;
+  };
+  //#endregion
 
-	const handlePageChange = (e, page) => {
-		setCurrentPage(page);
-	};
+  const handlePageChange = (e, page) => {
+    setCurrentPage(page);
+  };
 
-	return (
-		<Container style={{ marginTop: '20px' }}>
-			<Grid
-				container
-				direction="column"
-				justifyContent="center"
-				alignItems="center"
-			>
-				{/* 2 buttons */}
-				<Grid container alignItems="center" justifyContent="space-between">
-					<form>
-						<FormControl className={classes.formControl} variant="outlined">
-							<InputLabel id="places-list-category-label">Category</InputLabel>
-							<Select
-								defaultValue=""
-								label="Category"
-								autoWidth
-								labelId="places-list-category-label"
-							>
-								{renderCategories(categories)}
-							</Select>
-						</FormControl>
-					</form>
-					<Button
-						component={Link}
-						to="/places/new"
-						variant="outlined"
-						color="primary"
-						startIcon={<Add />}
-					>
-						<Box fontWeight={700}>Add a place</Box>
-					</Button>
-				</Grid>
+  const handleSearchSubmit = (formValues) => {
+    console.log(formValues);
+  };
 
-				{/* search bar  */}
-				<Grid></Grid>
-				{/* cards list */}
-				<Grid container alignItems="center">
-					{isLoading ? renderSkeletonPlaces() : renderPlaces()}
-				</Grid>
-				{/* pagination */}
-				<Grid>
-					<Pagination
-						color="primary"
-						hideNextButton
-						hidePrevButton
-						page={currentPage}
-						count={countPage()}
-						onChange={handlePageChange}
-					/>
-				</Grid>
-			</Grid>
-		</Container>
-	);
+  return (
+    <Container style={{ marginTop: '20px' }}>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        {/* 2 buttons */}
+        <Grid container alignItems="center" justifyContent="space-between">
+          <form>
+            <FormControl className={classes.formControl} variant="outlined">
+              <InputLabel id="places-list-category-label">Category</InputLabel>
+              <Select
+                defaultValue=""
+                label="Category"
+                autoWidth
+                labelId="places-list-category-label"
+              >
+                {renderCategories(categories)}
+              </Select>
+            </FormControl>
+          </form>
+          <Button
+            component={Link}
+            to="/places/new"
+            variant="outlined"
+            color="primary"
+            startIcon={<Add />}
+          >
+            <Box fontWeight={700}>Add a place</Box>
+          </Button>
+        </Grid>
+
+        {/* search bar  */}
+        <Grid container alignItems="center" style={{ marginTop: 20 }}>
+          <form autoComplete="off" onSubmit={handleSubmit(handleSearchSubmit)}>
+            <FormControl className={classes.formControl} variant="outlined">
+              <InputLabel id="search-place-label">Search place</InputLabel>
+              <OutlinedInput {...register('search')} label="Search place" />
+            </FormControl>
+          </form>
+        </Grid>
+        {/* cards list */}
+        <Grid container alignItems="center">
+          {isLoading ? renderSkeletonPlaces() : renderPlaces()}
+        </Grid>
+        {/* pagination */}
+        <Grid>
+          <Pagination
+            color="primary"
+            hideNextButton
+            hidePrevButton
+            page={currentPage}
+            count={countPage()}
+            onChange={handlePageChange}
+          />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
