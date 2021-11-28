6a93d62386858ddd2e6d753e069bde684dd66aee
khoatrn-dev <khoatrn.dev@gmail.com>
2021-11-28 15:05:56 +0700

    Updated apis' constants file - Added Image server url

--- src/apis/constants.js
+++ src/apis/constants.js
@@ -1,14 +1,16 @@
 // Constants.js
 const prod = {
-	url: {
-		API_URL: 'https://khoatrn-foodplace-v1.herokuapp.com/',
-	},
+  url: {
+    API_URL: 'https://khoatrn-foodplace-v1.herokuapp.com/',
+    IMAGE_SERVER_URL: 'http://localhost:5004',
+  },
 };
 const dev = {
-	url: {
-		API_URL: 'http://localhost:5000',
-		// API_URL: `https://khoatrn-foodplace-v1.herokuapp.com/`,
-	},
+  url: {
+    API_URL: 'http://localhost:5000',
+    IMAGE_SERVER_URL: 'http://localhost:5004',
+    // API_URL: `https://khoatrn-foodplace-v1.herokuapp.com/`,
+  },
 };
 // eslint-disable-next-line no-undef
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
