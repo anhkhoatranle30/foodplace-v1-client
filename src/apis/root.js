6192965700a588b880050650e6dfa7a3bd8c298f
khoatrn-dev <khoatrn.dev@gmail.com>
2021-11-28 15:06:43 +0700

    Updated apis' root file - Created imageRootApi

--- src/apis/root.js
+++ src/apis/root.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 import { config } from './constants';
 
 const rootApi = axios.create({
-	baseURL: config.url.API_URL,
+  baseURL: config.url.API_URL,
 });
 
 export default rootApi;
+
+export const imageRootApi = axios.create({
+  baseURL: config.url.IMAGE_SERVER_URL,
+});
